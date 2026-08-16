eaa507e716ee61f475f387bb5e71d08135d5805b
jewaniuk <jacob.ewaniuk@queensu.ca>
2026-08-16 13:28:23 -0400

    added allow dead code when loading in the C API bindings to avoid irrelevant warnings

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ use std::marker::PhantomData;
 #[allow(non_upper_case_globals)]
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
+#[allow(dead_code)]
 mod sys {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
